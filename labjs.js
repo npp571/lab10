4ee81f346a118bb306a9f69bfccc61701d421694
npp571 <npp57@njit.edu>
2024-12-03 22:47:47 -0500

    lab

---
+++ labjs.js
@@ -0,0 +1,31 @@
+function greetuser(){
+    let firstname = 'User';
+    let greeting = `Welcome, ${firstname}!`;
+    console.log(greeting)
+    document.getElementById("output").innerHTML = greeting
+}
+greetuser()
+
+function changeText(){
+    document.getElementById("message").innerText = "Hello, World!";
+}
+
+function addNumbers(){
+    let number1 = parseFloat(document.getElementById('number1').value);
+    let number2 = parseFloat(document.getElementById('number2').value);
+    let sum = number1 + number2;
+    document.getElementById('result').textContent = `Result = ${sum}`;
+}
+addNumbers()
+
+function updateTitle(){
+    let newTitle = document.getElementById(`inputfield`).value;
+    document.getElementById(`title`).textContext = newTitle;
+}
+
+function concatenateStrings(){
+    let str1 = document.getElementById(`string1`).value;
+    let str2 = document.getElementById(`string2`).value;
+    let result = str1 + str2;
+    document.getElementById(`combinedText`).textContent = result;
+}
